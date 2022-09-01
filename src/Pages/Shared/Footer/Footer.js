f9b0710d12cf60555f4cbca094d501d785179c32
Gandib Roy <gandibroy11@gmail.com>
2022-09-01 20:06:22 +0600

    Fotter page completed

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css';
 
 const Footer = () => {
+    const handleSubscribe = event => {
+        event.preventDefault();
+        event.target.reset();
+    }
     return (
-        <div>
-            <h2>footer</h2>
-        </div>
+        <footer className='footer-container mt-5'>
+            <div className='row'>
+                <form onSubmit={handleSubscribe} className='col-12 col-md-6 col-lg-6 subscribe-form mt-3'>
+                    <input type="email" name='email' placeholder='Enter Your Email' />
+                    <input type="submit" value="Subscribe" />
+                </form>
+                <div className='col-12 col-md-6 col-lg-6 mt-3'>
+                    <div><Link className='footer-pages' to='/login'>Login</Link></div>
+                    <div><Link className='footer-pages' to='/signup'>Signup</Link></div>
+                </div>
+            </div>
+            <p className='text-white mt-3 pb-3'>&copy; Copyright: Car Inventory {new Date().getFullYear()}</p>
+        </footer>
     );
 };
 
